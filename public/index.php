e9e51682d6be865cdebdb5e57fec219b7df7f813
func0der <529819+func0der@users.noreply.github.com>
2021-04-05 16:21:30 +0200

    [main] Simulate two applications with dedicated composer autoloaders.

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+require '../vendor/autoload.php';
+require '../third_party/SomeLib/vendor/autoload.php';
+
+new \Func0der\PhpStanTest\ClassExtendingSomeInterface();
--- src/ClassExtendingSomeInterface.php
+++ src/ClassExtendingSomeInterface.php
@@ -4,12 +4,30 @@ declare(strict_types=1);
 
 namespace Func0der\PhpStanTest;
 
+use JMS\Serializer\DeserializationContext;
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\SerializerInterface;
 use Some\Lib\SomeInterface;
 
-class ClassExtendingSomeInterface implements SomeInterface
+class ClassExtendingSomeInterface implements SerializerInterface, SomeInterface
 {
     public static function create(): self
     {
         return new self();
     }
+
+    public function serialize(
+        $data,
+        string $format,
+        ?SerializationContext $context = null,
+        ?string $type = null
+    ): string {
+        return '';
+    }
+
+    public function deserialize(string $data, string $type, string $format, ?DeserializationContext $context = null)
+    {
+        return new $type();
+    }
+
 }
